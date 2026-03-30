8a61a66d768b7bda4fc8d236331018bfb37cd8a9
Jose Lucas <drummond05@proton.me>
2026-03-30 09:25:14 -0300

    feat: setup uniwind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,28 @@
+import "../../global.css";
 
 import { Stack } from "expo-router";
+import { useCSSVariable } from "uniwind";
 
 export default function RootLayout() {
-    return (
-        <Stack>
-            <Stack.Screen name="index" />
-        </Stack>
-    )
+  const [headerBackground, foreground, background] = useCSSVariable([
+    "--header-background",
+    "--foreground",
+    "--background",
+  ]) as string[];
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: headerBackground,
+        },
+        headerTintColor: foreground,
+        headerShadowVisible: false,
+        contentStyle: {
+          backgroundColor: background,
+        },
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+    </Stack>
+  );
 }
