3e39de33549d9567c3980e32247d6fd522a20338
Jose Lucas <drummond05@proton.me>
2026-03-30 08:30:15 -0300

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+    return (
+        <View>
+            <Text className="text-xl text-red-500">Home</Text>
+        </View>
+    )
+}
