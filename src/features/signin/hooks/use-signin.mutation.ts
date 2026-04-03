64eb724e8aab43633fbb38b417174365872a28bc
Jose Lucas <drummond05@proton.me>
2026-04-03 16:17:17 -0300

    feat: implement show toast on success and error sign in

--- src/features/signin/hooks/use-signin.mutation.ts
+++ src/features/signin/hooks/use-signin.mutation.ts
@@ -2,12 +2,26 @@ import { useMutation } from "@tanstack/react-query";
 import { SignInRequest } from "../schemas/signin.request";
 import { apiClient } from "@/api/client";
 import { signinResponse } from "../schemas/signin.response";
+import { useToastStore } from "@/store/toast-store";
+import { AxiosError } from "axios";
 
 export function useSigninMutation() {
+  const { showToast } = useToastStore();
   return useMutation({
     mutationFn: async (data: SignInRequest) => {
       const res = await apiClient.post("/auth/login", data);
       return signinResponse.parse(res.data);
+    },
+    onSuccess: () =>
+      showToast({
+        description: "Login realizado com sucesso",
+        variant: "success",
+      }),
+    onError: (e) => {
+      if (e instanceof AxiosError) {
+        const message = e.response?.data.message ?? "Email/Senha inválidos";
+        showToast({ description: message, variant: "destructive" });
+      }
     },
   });
 }
