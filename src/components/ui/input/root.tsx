9bc9310a442970495b220d6fbb8baec335adbd4d
Jose Lucas <drummond05@proton.me>
2026-04-03 06:47:51 -0300

    style: implment border color animation on Input component

--- src/components/ui/input/root.tsx
+++ src/components/ui/input/root.tsx
@@ -1,7 +1,9 @@
 import { cn } from "@/lib/utils";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { ViewProps } from "react-native";
 import { StyledMotiView } from "../styled-moti-view";
+import { useAnimationState } from "moti";
+import { useCSSVariable } from "uniwind";
 
 const inputContext = createContext({
   isFocused: false,
@@ -13,13 +15,30 @@ export default function InputRoot({
   className,
   ...props
 }: ViewProps) {
+  const [borderColor, primaryColor] = useCSSVariable([
+    "--color-border",
+    "--color-primary",
+  ]) as string[];
   const [isFocused, setIsFocused] = useState(false);
+  const borderState = useAnimationState({
+    from: {
+      borderColor: borderColor,
+    },
+    focused: {
+      borderColor: primaryColor,
+    },
+  });
+
+  useEffect(() => {
+    borderState.transitionTo(isFocused ? "focused" : "from");
+  }, [borderState, isFocused]);
 
   return (
     <inputContext.Provider value={{ isFocused, setIsFocused }}>
       <StyledMotiView
+        state={borderState}
         className={cn(
-          "w-full rounded-base border border-border bg-input px-4 py-2 flex-row items-center gap-2 ",
+          `w-full rounded-base border-2 bg-input px-4 py-1 flex-row items-center gap-2`,
           className,
         )}
         {...props}
