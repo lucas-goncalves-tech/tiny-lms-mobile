c2237dd42d22e3119dcf55f427cfb03d62432223
Jose Lucas <drummond05@proton.me>
2026-04-03 08:54:09 -0300

    feat: implement themeProvider for app

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,37 +2,25 @@ import AuthInitializer from "@/components/providers/auth.initializer";
 import "../../global.css";
 
 import { Stack } from "expo-router";
-import { useCSSVariable } from "uniwind";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@react-navigation/native";
+import { useNavigationTheme } from "@/hooks/use-navigation-theme";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  const [headerBackground, foreground, background] = useCSSVariable([
-    "--header-background",
-    "--foreground",
-    "--background",
-  ]) as string[];
+  const theme = useNavigationTheme();
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthInitializer>
-        <Stack
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: headerBackground,
-            },
-            headerTintColor: foreground,
-            headerShadowVisible: false,
-            contentStyle: {
-              backgroundColor: background,
-            },
-          }}
-        >
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="(auth)/signin" options={{ headerShown: false }} />
-        </Stack>
-      </AuthInitializer>
+      <ThemeProvider value={theme}>
+        <AuthInitializer>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          </Stack>
+        </AuthInitializer>
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
---
+++ src/hooks/use-navigation-theme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme, Theme } from "@react-navigation/native";
+import { useCSSVariable } from "uniwind";
+
+export function useNavigationTheme(): Theme {
+  const [primary, background, card, text, border] = useCSSVariable([
+    "--primary",
+    "--background",
+    "--header-background",
+    "--foreground",
+    "--border",
+  ]) as string[];
+  return {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: primary || DefaultTheme.colors.primary,
+      background: background || DefaultTheme.colors.background,
+      card: card || DefaultTheme.colors.card,
+      text: text || DefaultTheme.colors.text,
+      border: border || DefaultTheme.colors.border,
+    },
+  };
+}
