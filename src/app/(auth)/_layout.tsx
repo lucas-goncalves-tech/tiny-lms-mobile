1ccb4b661610044ba1357d705de0cc63ab40676f
Jose Lucas <drummond05@proton.me>
2026-04-04 07:56:46 -0300

    feat: implmenet .agent for best study with AI and jakarta sans font

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,14 +1,24 @@
 import { useAuthStore } from "@/store/auth-store";
 import { Redirect, Stack } from "expo-router";
+import { useCSSVariable } from "uniwind";
 
 export default function AuthLayout() {
   const { isAuthenticated } = useAuthStore();
+  const [primary] = useCSSVariable(["--color-primary"]) as string[];
 
   if (isAuthenticated) return <Redirect href={"/home"} />;
   return (
     <Stack>
       <Stack.Screen name="signin" options={{ headerShown: false }} />
-      <Stack.Screen name="signup" />
+      <Stack.Screen
+        name="signup"
+        options={{
+          title: "Tiny LMS",
+          headerTitleStyle: {
+            color: primary,
+          },
+        }}
+      />
     </Stack>
   );
 }
--- src/hooks/use-navigation-theme.ts
+++ src/hooks/use-navigation-theme.ts
@@ -2,13 +2,15 @@ import { DefaultTheme, Theme } from "@react-navigation/native";
 import { useCSSVariable } from "uniwind";
 
 export function useNavigationTheme(): Theme {
-  const [primary, background, card, text, border] = useCSSVariable([
+  const [primary, background, card, text, border, fontSans] = useCSSVariable([
     "--primary",
     "--background",
     "--header-background",
     "--foreground",
     "--border",
+    "--font-sans",
   ]) as string[];
+
   return {
     ...DefaultTheme,
     colors: {
@@ -18,6 +20,13 @@ export function useNavigationTheme(): Theme {
       card: card || DefaultTheme.colors.card,
       text: text || DefaultTheme.colors.text,
       border: border || DefaultTheme.colors.border,
+    },
+    fonts: {
+      ...DefaultTheme.fonts,
+      regular: { fontFamily: fontSans, fontWeight: "400" },
+      medium: { fontFamily: fontSans, fontWeight: "500" },
+      bold: { fontFamily: fontSans, fontWeight: "700" },
+      heavy: { fontFamily: fontSans, fontWeight: "900" },
     },
   };
 }
