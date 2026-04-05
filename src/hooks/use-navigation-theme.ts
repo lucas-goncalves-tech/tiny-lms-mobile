de2a5223963374e6c58e3512561b3fa6da4d1267
Jose Lucas <drummond05@proton.me>
2026-04-05 12:14:36 -0300

    fix: not using useMemo for navigation theme hook and causing unecessary re-renders

--- src/hooks/use-navigation-theme.ts
+++ src/hooks/use-navigation-theme.ts
@@ -1,25 +1,29 @@
 import { useUniwindCSS } from "@/components/providers/color-provider";
 import { DefaultTheme, Theme } from "@react-navigation/native";
+import { useMemo } from "react";
 
 export function useNavigationTheme(): Theme {
   const theme = useUniwindCSS();
 
-  return {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: theme.primary || DefaultTheme.colors.primary,
-      background: theme.background || DefaultTheme.colors.background,
-      card: theme.headerBackground || DefaultTheme.colors.card,
-      text: theme.foreground || DefaultTheme.colors.text,
-      border: theme.border || DefaultTheme.colors.border,
-    },
-    fonts: {
-      ...DefaultTheme.fonts,
-      regular: { fontFamily: theme.fontSans, fontWeight: "400" },
-      medium: { fontFamily: theme.fontSans, fontWeight: "500" },
-      bold: { fontFamily: theme.fontSans, fontWeight: "700" },
-      heavy: { fontFamily: theme.fontSans, fontWeight: "900" },
-    },
-  };
+  return useMemo(
+    () => ({
+      ...DefaultTheme,
+      colors: {
+        ...DefaultTheme.colors,
+        primary: theme.primary || DefaultTheme.colors.primary,
+        background: theme.background || DefaultTheme.colors.background,
+        card: theme.headerBackground || DefaultTheme.colors.card,
+        text: theme.foreground || DefaultTheme.colors.text,
+        border: theme.border || DefaultTheme.colors.border,
+      },
+      fonts: {
+        ...DefaultTheme.fonts,
+        regular: { fontFamily: theme.fontSans, fontWeight: "400" },
+        medium: { fontFamily: theme.fontSans, fontWeight: "500" },
+        bold: { fontFamily: theme.fontSans, fontWeight: "700" },
+        heavy: { fontFamily: theme.fontSans, fontWeight: "900" },
+      },
+    }),
+    [theme],
+  );
 }
